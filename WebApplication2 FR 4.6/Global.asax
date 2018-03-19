75677fa4e1792ce11c3fd6389d5559563b8fdbf9
carloscespedes <cespedescarlos0610@gmail.com>
2018-03-19 11:02:25 -0400

    commit2

---
+++ WebApplication2 FR 4.6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication2_FR_4._6.MvcApplication" Language="C#" %>
---
+++ WebApplication2 FR 4.6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication2_FR_4._6.Startup))]
+namespace WebApplication2_FR_4._6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
